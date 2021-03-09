9861c7bb5063cc4194af7ca8679097e82c0fcf02
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-09 00:37:57 -0300

    Fazendo correspondência com vários prefixos.

--- Chapter 7 - Regex/heroRegex.py
+++ Chapter 7 - Regex/heroRegex.py
@@ -3,6 +3,9 @@ import re
 # Fazendo correspondência de vários grupos com pipe |
 heroRegex = re.compile(r'Batman|Tina Fey')
 
+# Fazendo correspondência com vários prefixos
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+
 if __name__ == '__main__':
     # Fazendo correspondência de vários grupos com pipe |
     moHero1 = heroRegex.search('Batman and Tina Fey.')
@@ -10,3 +13,8 @@ if __name__ == '__main__':
 
     moHero2 = heroRegex.search('Tina Fey and Batman')
     print(moHero2.group())
+
+    # Fazendo correspondência com vários prefixos
+    moBat = batRegex.search('Batmobile lost a wheel')
+    print(moBat.group())
+    print(moBat.group(1))
